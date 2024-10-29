9dbd4b74ce3964367f79109cbe75d786071ed6b0
cuzethan <leethan444@gmail.com>
2024-10-28 23:49:23 -0700

    initalized project

---
+++ borderify/src/main/java/com/ethan/borderify/BorderifyApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.borderify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorderifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorderifyApplication.class, args);
+	}
+
+}
---
+++ borderify/src/test/java/com/ethan/borderify/BorderifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.borderify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorderifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
